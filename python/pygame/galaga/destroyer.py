f1bc6d9021c05a29bf5eb0ffb68716d6fa196a7f
pujangga123 <rush_x@yahoo.com>
2022-06-08 17:03:16 +0700

    Update destroyer.py

--- python/pygame/galaga/destroyer.py
+++ python/pygame/galaga/destroyer.py
@@ -5,6 +5,3 @@ class Destroyer(Drone):
     def __init__(self,screen):
         Drone.__init__(self,screen)
         self.img_drone = pygame.image.load("destroyer.png")
-        self.speed = 3
-        self.screen = screen
-        self.active = True
